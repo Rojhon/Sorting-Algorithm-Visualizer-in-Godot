8febdebcaa369409ad057d31f821f19e5c64199c
RojhonBB <buenaventurarojhon5@gmail.com>
2021-07-12 16:46:12 +0800

    Generate Random Integer Array Value with slider

--- Scene/Script/MainScene.cs
+++ Scene/Script/MainScene.cs
@@ -1,21 +1,81 @@
 using Godot;
 using System;
+using System.Linq;
 
 public class MainScene : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
+    public HSlider arraySizeSlider;
     public override void _Ready()
     {
-        
+        arraySizeSlider = this.GetNode<HSlider>("Control/ArraySizeSlider");
+        // GD.Print(string.Join(",", RandomArrayValue((int)arraySizeSlider.MinValue)));
+
+        InitArray((int)arraySizeSlider.Value);
+
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+// Return a Set of Random Integer Value in Array
+    public int[] RandomArrayValue(int arraySize){
+        int[] value = new int[arraySize];
+        int[] newValue = new int[arraySize];
+
+        int randomValue;
+        Random rn = new Random();
+
+        for(int i = 0; i < value.Length; i++){
+            randomValue = rn.Next((int)arraySizeSlider.MinValue, (int)arraySizeSlider.MaxValue + 1);
+            value[i] = randomValue;
+            newValue = value.Distinct().ToArray();
+
+        }
+
+        return newValue;
+
+    }
+
+    private void InitArray(int arraySize){
+        int[] arr = new int[arraySize];
+        Random rnd = new Random();
+        int temp;
+
+        for(int i = 0; i < arr.Length; i++){
+            temp = rnd.Next(arraySize + 1);
+            while(IsDup(temp, arr)){
+                temp = rnd.Next(arraySize + 1);
+
+            }
+            arr[i] = temp;
+        }
+
+        PrintArr(arr);
+       
+    }
+
+    public void PrintArr(int[] arr){
+         GD.Print(string.Join(", ", arr));
+
+    }
+
+    public bool IsDup(int temp, int[] arr){
+        foreach(var item in arr){
+            if(item == temp){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void _on_ArraySizeSlider_value_changed(int value){
+        // GD.Print(string.Join(",", RandomArrayValue(value)));
+        InitArray((int)arraySizeSlider.Value);
+
+    }
+
+// Return a Set of Random Integer Value in Array when Pressed
+    public void _on_GenerateNewArray_pressed(){
+        // GD.Print(string.Join(",", RandomArrayValue((int)arraySizeSlider.Value)));
+        InitArray((int)arraySizeSlider.Value);
+    }
+
 }
