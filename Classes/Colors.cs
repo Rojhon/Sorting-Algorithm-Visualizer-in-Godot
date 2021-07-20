5fca25fe1ca0a879e9750fd13f5cfb281eefcdc8
RojhonBB <buenaventurarojhon5@gmail.com>
2021-07-20 08:22:31 +0800

    Color

--- Classes/Colors.cs
+++ Classes/Colors.cs
@@ -4,7 +4,7 @@ public class Colors
 {
     public static Color defaulColor = new Color( 1, 1, 1, 1 );
     public static Color sortedColor = Color.Color8(114, 255, 122, 255);
-    public static Color comparingColor = new Color( 0, 0, 1, 1 ) ;
-    public static Color swappingColor = new Color( 1, 0, 0, 1 );
+    public static Color comparingColor = Color.Color8(80, 140, 255, 255) ;
+    public static Color swappingColor = Color.Color8( 255, 99, 99, 255);
 
 }
